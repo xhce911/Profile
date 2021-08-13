183d7e0879970a09d6e051cf7bee7ce2ac126b70
Thce911 <11hce@tec.mx>
2021-08-12 22:07:07 -0500

    fix pt 1

--- server.js
+++ server.js
@@ -1,16 +1,9 @@
 const express = require ('express')
 const Page = require('./models/pages')
-const mongoose = require('mongoose')
 const pageRouter = require('./routes/pages')
-const methodOverride = require('method-override')
 const app = express()
 const port = 5001;
 
-mongoose.connect('mongodb://localhost/book',  {
-    useUnifiedTopology: true,  
-    useNewUrlParser: true,
-    useCreateIndex: true
-})
 
 app.set('view engine', 'ejs')
 app.use(express.urlencoded({extended: false}))
@@ -22,17 +15,13 @@ app.use('/img', express.static(__dirname + 'public/img'));
 app.use('/files', express.static(__dirname + 'public/files'));
 
 
-app.use(methodOverride('_method'))
-
 app.get('/', async (req, res ) => {
-    const pages = await Page.find().sort({
-        logDate: 'desc'
-    })
     res.render('pages/index', { pages: pages})
 })
 
 
 app.use('/pages', pageRouter)
+
 app.listen(process.env.PORT || port,
     () => console.info(`Listening on port ${port}`));
 
