d575d3e38f82563a3f72d72bcc3cbc3eb2ca2acd
Thce911 <11hce@tec.mx>
2021-08-13 14:56:50 -0500

    fix issues

--- server.js
+++ server.js
@@ -1,12 +1,7 @@
 const express = require ('express')
-const Page = require('./models/pages')
-const pageRouter = require('./routes/pages')
 const app = express()
 const port = 5001;
 
-
-app.set('view engine', 'ejs')
-app.use(express.urlencoded({extended: false}))
 
 app.use(express.static('public'));
 app.use('/css', express.static(__dirname + 'public/css'));
@@ -14,13 +9,13 @@ app.use('/js', express.static(__dirname + 'public/js'));
 app.use('/img', express.static(__dirname + 'public/img'));
 app.use('/files', express.static(__dirname + 'public/files'));
 
-
-app.get('/', async (req, res ) => {
-    res.render('pages/index', { pages: pages})
-})
+app.use(express.static('views'));
+app.use("/views", express.static(__dirname + "views/"));
 
 
-app.use('/pages', pageRouter)
+app.get("/", (req, res) =>{
+    res.sendFile(__dirname + "/views/index.html");
+});
 
 app.listen(process.env.PORT || port,
     () => console.info(`Listening on port ${port}`));
