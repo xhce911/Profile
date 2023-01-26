837a4b017cbe75b461748a651955ca8b6e6911d1
HCE <xhce911@gmail.com>
2023-01-25 23:56:22 -0600

    Updating portfolio

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const port = 5001;
 
 
 app.use(express.static('public'));
+
 app.use('/css', express.static(__dirname + 'public/css'));
 app.use('/js', express.static(__dirname + 'public/js'));
 app.use('/img', express.static(__dirname + 'public/img'));
